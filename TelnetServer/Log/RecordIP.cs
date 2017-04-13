d46a841d830640e450471c7406027af39f3a38be
gege <gege@DESKTOP-SJO31SN>
2017-04-13 17:12:43 +0800

    1`234

---
+++ TelnetServer/Log/RecordIP.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelnetServer.Models;
+
+namespace TelnetServer.Log
+{
+    public static class RecordIP
+    {
+
+        public static void recordip(Ip ip)
+        {
+            using (var TelnetDbContext = new TelnetDbContext())
+            {
+                bool res = TelnetDbContext.Database.CreateIfNotExists();
+                TelnetDbContext.Ip.Add(ip);
+                TelnetDbContext.SaveChanges();
+
+            }
+        }
+    }
+}
